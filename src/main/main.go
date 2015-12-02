1f67f71fa711df4ad57bd5b2b253dc2543ad69f0
sunxiaojun-xy <sunxiaojun-xy@w31.lk.corp.qihoo.net>
2015-12-02 18:14:15 +0800

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -10,7 +10,7 @@ func httptest() {
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello,%q", html.EscapeString(r.URL.Path))
 	})
-	http.ListenAndServe(":5000", nil)
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
